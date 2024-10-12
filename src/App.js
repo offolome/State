eeebda25d857f24d775617e20ace63730aee79e4
unknown <offado>
2024-10-12 15:30:07 +0000

    Exo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+ constructor(props){
+    super(props);
+    this.state = {
+      personne : {
+        fullName: "Adon jonathan",
+        bio: "Besoin d'un développeur freelance contactez-moi au numéro suivant: 07 87 60 67 08",
+        imgSrc: "https://db0dce98.rocketcdn.me/wp-content/uploads/2024/02/developpeur-web-datascientest-1024x585.jpg",
+        profession: "Développeur fullstack"
+      },
+
+      show: true,
+
+      count: 0
+    }
+ }
+
+ handleProfile = () => {
+  this.setState({
+    show: !this.state.show,
+    count: 0
+  })
+ }
+
+ componentDidMount() {
+  this.timer = setInterval(() => {
+    this.setState((prevState) => ({
+      count: prevState.count+1
+    }))
+  }, 1000);
+ }
+
+ componentWillUnmount() {
+  clearInterval(this.timer);
+ }
+
+ render() {
+  const {fullName, bio, imgSrc, profession} = this.state.personne;
+
+  return(
+    <>
+      <div>
+        {this.state.show && (<div>
+          <p>{fullName}</p>
+          <p>{bio}</p>
+          <img src={imgSrc}  alt='photo' />
+          <p>{profession}</p>
+        </div>)}
+
+        <button onClick={this.handleProfile}> Changer </button>
+      </div>
+
+      <div>
+        {!this.state.show && <h3>{this.state.count}</h3>}          
+      </div>
+    </>
   );
+ }
 }
 
 export default App;
